4dc3fbdbcee5d22be6d177ee02c18209b9568952
Vlt09 <valentin.barrau@free.fr>
2026-07-11 23:39:44 +0100

    feat: add request

--- app/src/main/java/com/vlt/zeroperte/model/FoodDao.kt
+++ app/src/main/java/com/vlt/zeroperte/model/FoodDao.kt
@@ -5,12 +5,29 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import java.time.LocalDate
 
 @Dao
 interface FoodDao {
 
     @Query("SELECT * FROM food WHERE id IN (:foodId)")
     fun findById(foodId: Long): Food?
+
+    @Query("SELECT * FROM food WHERE name LIKE :name")
+    fun findByName(name: String): List<Food>
+
+    @Query("SELECT * FROM food WHERE category = :category")
+    fun findByCategory(category: String): List<Food>
+
+    @Query("SELECT * FROM food WHERE brand = :brand")
+    fun findByBrand(brand: String): List<Food>
+
+    @Query("SELECT * FROM food WHERE expiryDate >= date()")
+    fun findExpired() : List<Food>
+
+    @Query("SELECT * FROM food WHERE expiryDate >= date('now')" +
+            "AND julianday(expiryDate) - julianday('now') < :days")
+    fun findExpiringSinceDays(days: Int) : List<Food>
 
     @Query("SELECT * FROM food")
     fun allFoods(): List<Food>
