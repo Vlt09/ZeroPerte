f6573b3295b46c8d0db4c6d0b3d85d95c951ddb7
Vlt09 <valentin.barrau@free.fr>
2026-07-16 11:33:59 +0100

    feat: implements uiState in FoodListViewModel.kt

--- app/src/main/java/com/vlt/zeroperte/ui/FoodListViewModel.kt
+++ app/src/main/java/com/vlt/zeroperte/ui/FoodListViewModel.kt
@@ -7,10 +7,12 @@ import com.vlt.zeroperte.data.model.FoodListViewModelDto
 import com.vlt.zeroperte.data.model.domain.FoodStatus
 import com.vlt.zeroperte.utils.FoodMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
@@ -19,72 +21,89 @@ import java.util.Optional
 import javax.inject.Inject
 
 
-sealed interface FoodListUiState{
-    data object Loading : FoodListUiState
-    data object Empty : FoodListUiState
-
-    data class Error(val message : String) : FoodListUiState
-
-    data class Content(val foods: List<FoodListViewModelDto>)
-        : FoodListUiState
-}
-
-sealed interface FoodFilterUiState{
-    data class FilterState(val statuses : List<FoodStatus>,
-                           val selectedStatus: Optional<FoodStatus>)
-}
 
 @HiltViewModel
 class FoodListViewModel @Inject constructor(private val repository: FoodRepository): ViewModel() {
 
+    sealed interface FoodListUiState{
+        data class Empty(val filter: FilterState) : FoodListUiState
 
-    private val _uiState = MutableStateFlow<FoodListUiState>(FoodListUiState.Empty)
-    val uiState = _uiState.asStateFlow()
+        data class Content(val foods: List<FoodListViewModelDto>,
+                           val filter: FilterState)
+            : FoodListUiState
 
-    private fun fetchAllFoods() = viewModelScope.launch {
-        _uiState.update { FoodListUiState.Loading }
-        repository.getAllFoods()
-            .collect { foods ->
-                if (foods.isEmpty()) {
-                    _uiState.update {  FoodListUiState.Empty }
-                } else {
-                    _uiState.update {
-                        FoodListUiState.Content(
-                            foods.map { f ->
-                                FoodMapper.toFoodListViewModelDto(f)
-                            }.toList()
-                        )
-                    }
-                }
-            }
+        data object Loading : FoodListUiState
     }
+
+    data class FilterState(val statuses : List<FoodStatus>,
+                           val selectedStatus: FoodStatus?)
+
+
+    private val _foodListFlow: Flow<List<FoodListViewModelDto>> =
+        repository.getAllFoods()
+            .map { foods -> foods.map { f -> FoodMapper.toFoodListViewModelDto(f) }.toList() }
+
 
     // Backing property to avoid state updates from other classes
     private val _filterUiState = MutableStateFlow(
-        FoodFilterUiState.FilterState(
+        FilterState(
                     statuses = FoodStatus.allStatuses,
-                    selectedStatus = Optional.empty<FoodStatus>()
+                    selectedStatus = null
                     )
                 )
 
     val filterUiState =  _filterUiState.asStateFlow()
+
+    val uiState : StateFlow<FoodListUiState> =
+        combine(
+            _foodListFlow,
+            _filterUiState
+        ){
+            foods, filter ->
+            if (foods.isEmpty()){
+                FoodListUiState.Empty(filter)
+            }
+            else{
+                if (filter.selectedStatus != null){
+                    val filteringFoods = foods.filter { f -> f.status == filter.selectedStatus }.toList()
+                    // There is not food that match with the filter so it's empty
+                    if (filteringFoods.isEmpty()){
+                        FoodListUiState.Empty(filter)
+                    }
+                    else{
+                        FoodListUiState.Content(filteringFoods,
+                            filter)
+                    }
+                }
+                else{
+                    FoodListUiState.Content(foods, filter)
+                }
+            }
+
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = FoodListUiState.Loading,
+        )
+
+
 
     fun toggleStatus(status: FoodStatus){
         _filterUiState.update {
                 val currentSelectedStatus = it.selectedStatus
                 val newStatus =
                     // Filter selected twice is cancelled
-                    if (currentSelectedStatus.isPresent && currentSelectedStatus.get() == status
-                        || currentSelectedStatus.isEmpty){
-                    Optional.empty<FoodStatus>()
+                    if (currentSelectedStatus == status){
+                    null
                 }
                 else {
-                    Optional.of<FoodStatus>(status)
+                    status
                 }
                 return@update it.copy(
                     selectedStatus = newStatus
                 )
         }
     }
+
 
 }
