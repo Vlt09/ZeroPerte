8d04208a7c6cd86e901e171a4a4e0071be18df28
Vlt09 <valentin.barrau@free.fr>
2026-08-04 23:58:22 +0100

    feat: FoodCreateUpdateViewModel.kt save function

--- app/src/main/java/com/vlt/zeroperte/ui/FoodCreateUpdateViewModel.kt
+++ app/src/main/java/com/vlt/zeroperte/ui/FoodCreateUpdateViewModel.kt
@@ -2,14 +2,43 @@ package com.vlt.zeroperte.ui
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.vlt.zeroperte.data.FoodRepository
+import com.vlt.zeroperte.data.model.FoodDto
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.Date
 import javax.inject.Inject
 
-class FoodCreateUpdateViewModel @Inject constructor() : ViewModel() {
-
+@HiltViewModel
+class FoodCreateUpdateViewModel @Inject constructor(private val repository: FoodRepository) : ViewModel() {
     var form = FoodForm()
 
     fun validate() {
         form.validate(true)
         Log.d("FoodCreateUpdateViewModel", "Validate (form is valid: ${form.isValid})")
+    }
+
+    fun dateToLocalDate(date: Date): LocalDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+
+    /*Faire un état qui indique l'état de l'enregistrement*/
+
+    suspend fun save(){
+        form.validate(markAsChanged = true)
+        if (form.isValid){
+            val values = form.getRawValues()
+
+            val foodDto = FoodDto(
+                name = values["name"] as String,
+                expiryDate = dateToLocalDate(values["expiryDate"] as Date),
+                brand = values["brand"] as String,
+                category = values["category"] as String,
+                datePurchased = dateToLocalDate(values["datePurchased"] as Date),
+                comment = values["comment"] as String,
+                amount = values["amount"] as Int
+            )
+
+            repository.create(foodDto)
+        }
     }
 }
